2a9e323d458a0323df20efc01a0e82744459a65c
LewisHawkins <35339211+LewisHawkins@users.noreply.github.com>
2020-03-06 12:44:02 +0000

    Created the base mathdoku file

---
+++ Mathdoku.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+
+
+public class Mathdoku extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start (Stage stage) {
+        // Set the title of the GUI
+        stage.setTitle("Mathdoku");
+    }
+}
